b5268e6e007fd3a46b1b5705a5c0315d2fe34897
Leonardo Guerra <leonardo.guerra@air-srl.com>
2021-01-04 11:46:03 +0100

    UPD eq_tommaso_sale_delivery_date_website_31_12_20

--- eq_tommaso_sale_delivery_date_website/static/src/js/main.js
+++ eq_tommaso_sale_delivery_date_website/static/src/js/main.js
@@ -2,12 +2,14 @@ odoo.define('eq_tommaso_sale_delivery_date_website.product_display_delivery_days
     "use strict";
 
     var ajax = require('web.ajax');
+    var core = require('web.core');
+    var _t = core._t;
 
     $(document).ready(function (ev) {
         ajax.jsonRpc("/get_product_delivery_days", 'call', {'product_id':$('.product_id').val()})
             .then(function(data) {
                 if (data){
-                    $(".delivery_days").text('Usually Delivered in ' + data + ' Days');
+                    $(".delivery_days").text(_t('Usually Delivered in ') + data + _t(' Days'));
                 }
                 else{
                     $(".delivery_days").text('');
@@ -18,7 +20,7 @@ odoo.define('eq_tommaso_sale_delivery_date_website.product_display_delivery_days
             ajax.jsonRpc("/get_product_delivery_days", 'call', {'product_id':$('.product_id').val()})
             .then(function(data) {
                 if (data){
-                    $(".delivery_days").text('Usually Delivered in ' + data + ' Days');
+                    $(".delivery_days").text(_t('Usually Delivered in ') + data + _t(' Days'));
                 }
                 else{
                     $(".delivery_days").text('');
